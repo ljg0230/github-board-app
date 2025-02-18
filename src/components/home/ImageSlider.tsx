434788d78a8450fafdc121a0ade07c7ba0fdfaaa
ljg0117 <ljg0117@greencar.co.kr>
2025-02-18 18:24:53 +0900

    feat: swiper image section 추가

--- src/components/home/ImageSlider.tsx
+++ src/components/home/ImageSlider.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const images = [
+  'https://picsum.photos/800/300',
+  'https://picsum.photos/800/300',
+  'https://picsum.photos/800/300',
+  'https://picsum.photos/800/300',
+];
 
 const ImageSlider: React.FC = () => {
   return (
@@ -7,11 +19,33 @@ const ImageSlider: React.FC = () => {
         width: '100%',
         height: '300px',
         backgroundColor: '#f8f9fa',
-        marginBottom: '20px'
+        marginBottom: '50px',
+        position: 'relative'
       }}
     >
-      {/* Swiper 컴포넌트는 나중에 구현 */}
-      이미지 슬라이더 영역
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={src}
+              alt={`Slide ${index + 1}`}
+              style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
